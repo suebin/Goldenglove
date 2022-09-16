70dbf1d88d3add9ab6df0109065339882fca65f0
suebin <suuebin@gmail.com>
2022-09-16 10:46:50 +0900

    test

--- src/main/java/com/example/golden_glove/GoldenGloveApplication.java
+++ src/main/java/com/example/golden_glove/GoldenGloveApplication.java
@@ -3,6 +3,7 @@ package com.example.golden_glove;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 테스트
 @SpringBootApplication
 public class GoldenGloveApplication {
 
