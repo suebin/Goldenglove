ebf56a74e02e459dbb0c5347b81413192a87749e
suebin <suuebin@gmail.com>
2022-09-20 09:35:09 +0900

    teammatch 추가, application.properties 제외

--- src/main/java/com/example/golden_glove/GoldenGloveApplication.java
+++ src/main/java/com/example/golden_glove/GoldenGloveApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
+
+@SpringBootApplication
+
 @ComponentScan
 @ComponentScan(basePackages = "main")
 @ComponentScan(basePackages = "user")
