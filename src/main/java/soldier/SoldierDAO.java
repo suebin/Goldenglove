86258e5eb8f57655ac2d75197548b18dfffdd7c3
suebin <suuebin@gmail.com>
2022-10-18 17:18:03 +0900

    soldier

---
+++ src/main/java/soldier/SoldierDAO.java
@@ -0,0 +1,10 @@
+package soldier;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository("soldierdao")
+public interface SoldierDAO {
+
+}
